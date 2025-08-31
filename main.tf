44927ef8cee93f3553408837db385a16ca6d6c91
devendraSinghtomar24 <99275680+devendraSinghtomar24@users.noreply.github.com>
2025-08-31 08:34:09 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.41.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "manoj_rg"          
+    storage_account_name =  "manojstr06"                            
+    container_name       = "manojcontainer"                            
+    key                  = "devops.tfstate"               
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+  subscription_id = "e08e3abd-807e-4b84-8dff-5647daf004e3" 
+}
+
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "samsung-rg-03"
+  location = "West Europe"
+}
